8ee8438ce6171727d6ec8f0953a72dcf34c2eb20
sundeep <slowlymakesyouperfect@gmail.com>
2024-02-02 21:25:51 +0530

    ok

--- src/app/student-registration/api/del/route.js
+++ src/app/student-registration/api/del/route.js
@@ -1,11 +1,11 @@
 import {NextResponse} from "next/server"
 import {verify} from "jsonwebtoken"
-import {DeleteStudent} from "../../model/students"
+import {deleteStudent} from "../../model/students"
 export async function GET(req){
    try{
     let cookie = await req.cookies.get("jwt")
     await verify(cookie.value,process.env.SECRET)
-    let res = await DeleteStudent(req.nextUrl.searchParams.get('id'))
+    let res = await deleteStudent(req.nextUrl.searchParams.get('id'))
     return NextResponse.json({success:true,message:res},{status:200})
    }catch(err){
     return NextResponse.json({error:true,message:err.message},{status:501})
