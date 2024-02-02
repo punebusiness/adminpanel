de26c12ba54bf5776eb14a7e86fd76c605802a74
sundeep <slowlymakesyouperfect@gmail.com>
2024-02-02 21:20:05 +0530

    ok

--- src/app/api/conn.js
+++ src/app/api/conn.js
@@ -1,17 +1,20 @@
-import mysql from "mysql"
-export default function cdb(){
+import mysql from "mysql";
+
+export default function cdb() {
     let con = mysql.createConnection({
-      host:process.env.HOST,
-      user:process.env.USER,
-      password:process.env.PASS,
-      database:process.env.DBNAME
-    })
-    con.connect(err=>{
-      if(err){
-        console.log(err);
-      }
-      console.log("database connected!")
-    })
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: process.env.DBNAME,
+        port: 3312
+    });
+
+    con.connect(err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("database connected!");
+    });
 
     return con;
 }
--- src/app/student-registration/api/approve/route.js
+++ src/app/student-registration/api/approve/route.js
@@ -1,11 +1,11 @@
 import {NextResponse} from "next/server"
 import {verify} from "jsonwebtoken"
-import {ApproveStudent} from "../../model/students"
+import {approveStudent} from "../../model/students"
 export async function GET(req){
 try{
     let cookie = await req.cookies.get("jwt")
     await verify(cookie.value,process.env.SECRET)
-    let stats = await ApproveStudent(req.nextUrl.searchParams.get('id'))
+    let stats = await approveStudent(req.nextUrl.searchParams.get('id'))
     console.log(stats);
     return NextResponse.json({success:true,message:stats},{status:200})
 
