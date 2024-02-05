f9cca4cb7999a65dec7826dfe20345f2427e31d9
punebusiness <154752916+punebusiness@users.noreply.github.com>
2024-02-05 18:22:43 +0530

    Update conn.js

--- src/app/api/conn.js
+++ src/app/api/conn.js
@@ -3,11 +3,11 @@ import mysql from "mysql2/promise";
 export default async function cdb() {
     try {
         const con = await mysql.createConnection({
-            host: "monorail.proxy.rlwy.net",
-            user: "root",
-            password: "FhEc2h5A4Ce-cbc5ag15dG44behDfF-b",
-            database: "railway",
-            port: 55609,
+            host: "103.143.46.252",
+            user: "jnps23_admin",
+            password: "Deepak@3370",
+            database: "jnps23_admin",
+            port:2082,
         });
 
         console.log("Database connected!");
