4c75cd5157147d499b500169f0a7879005bc1b82
punebusiness <154752916+punebusiness@users.noreply.github.com>
2024-02-05 18:28:03 +0530

    Update conn.js

--- src/app/api/conn.js
+++ src/app/api/conn.js
@@ -4,8 +4,8 @@ export default async function cdb() {
     try {
         const con = await mysql.createConnection({
             host: "103.143.46.252",
-            user: "jnps23_admin",
-            password: "Deepak@3370",
+            user: "jnps23_ndb",
+            password: "}cB~+5mKV@vviHF",
             database: "jnps23_admin",
             port:2082,
         });
